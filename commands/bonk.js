4f27d36253bdff5cfd45d97695da701b55b07cdc
Jacobtread <jacobtread@gmail.com>
2024-09-29 00:23:40 +1200

    feat: bonk command

---
+++ commands/bonk.js
@@ -0,0 +1,22 @@
+(() => {
+    // List of people that have already been bonked (Contains an entry for the person every time they've been bonked)
+    const bonkList = `$(urlfetch json https://twitch.center/customapi/quote/list?token={{REPLACE_ENV_BONK_PUBLIC_KEY}}&no_id=1)`
+        .split(`,`);
+
+    // Triggers the API that will update the counter to add the new entry
+    const _ = `$(urlfetch https://twitch.center/customapi/addquote?token={{REPLACE_ENV_BONK_PRIVATE_KEY}}&data=$(touser),)`;
+
+    // Number of times that user has been bonked
+    let bonkCount = 0;
+
+    for (var i = 0; i < bonkList.length; i += 1) {
+        const value = bonkList[i];
+
+        // Increase counter for every time the target user is found
+        if (value === `$(touser)`) {
+            bonkCount++;
+        }
+    }
+
+    return `-----|_| $(user) has bonked $(touser). $(touser) has been bonked ${bonkCount + 1} times.`;
+})();
