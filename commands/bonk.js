e620376a67d1ef50fa7d85b774b02f1addf36387
Jacobtread <jacobtread@gmail.com>
2024-10-09 21:28:52 +1300

    fix: bonk command use json fetch to bypass api character limit, strip junk characters from list items

--- commands/bonk.js
+++ commands/bonk.js
@@ -13,11 +13,11 @@
  * -----|_| TestFromUser has bonked TestToUser. TestToUser has been bonked 12 times.
  */
 
-const { urlfetch } = require("./lib/urlfetch");
+const { urlfetchJSON } = require("./lib/urlfetch");
 
 // List of people that have already been bonked (Contains an entry for the person every time they've been bonked)
 const bonkList = (
-  await urlfetch(
+  await urlfetchJSON(
     "https://twitch.center/customapi/quote/list?token={{REPLACE_ENV_BONK_PUBLIC_KEY}}&no_id=1"
   )
 ).split(`,`);
@@ -28,11 +28,13 @@ const _ = `$(urlfetch https://twitch.center/customapi/addquote?token={{REPLACE_E
 // Number of times that user has been bonked
 let bonkCount = 0;
 
+const actualUser = `$(touser)`.toLowerCase();
+
 for (var i = 0; i < bonkList.length; i += 1) {
-  const value = bonkList[i];
+  const value = bonkList[i].replace(/\n/g, "").toLowerCase();
 
   // Increase counter for every time the target user is found
-  if (value === `$(touser)`) {
+  if (value === actualUser) {
     bonkCount++;
   }
 }
